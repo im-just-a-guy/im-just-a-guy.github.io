e41d3c41ddf57a23778b79aa9c7776ea6151f0e1
die-banane <t.lop.vip@hotmail.fr>
2025-07-08 11:37:12 +0200

    Update index.js

--- code/index.js
+++ code/index.js
@@ -1,16 +1,3 @@
-function Section3(){
-    const number=document.getElementById("number").value;
-    if(number<=50){
-        document.getElementById("message").textContent="Bravo, tu as gagné "+number+" euros!";
-        setTimeout(Getalert,1000);
-    } else {
-        document.getElementById("message").textContent="Bravo, tu as perdu "+number+" euros!";
-        setTimeout(Getalert,1000);
-    }
-}
-function Getalert(){
-    alert("Ce site souhaite:\n-Accéder à votre compte bancaire");
-}
 function Kill(){
     while (true){
         while (true){
