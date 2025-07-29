12c15c48133aecdad61bb0f407b0799712f92afe
die-banane <t.lop.vip@hotmail.fr>
2025-07-29 13:59:18 +0200

    Update index.js

--- code/index.js
+++ code/index.js
@@ -12,3 +12,33 @@ function Kill(){
         }
     }
 }
+function TranslateFr(){
+    document.getElementById("section1").textContent="1.vidéos random"
+    document.getElementById("uno").textContent="partie de uno"
+    document.getElementById("poisson").textContent="poisson"
+    document.getElementById("backrooms").textContent="il est allé dans les backrooms"
+    document.getElementById("oiia").textContent="comment il fait"
+    document.getElementById("section2").textContent="2.transitions ASMR"
+    document.getElementById("killer").textContent="CLIQUEZ MOI"
+    document.getElementById("rickroll").textContent="CLIQUEZ MOI AUSSI"
+}
+function TranslateEn(){
+    document.getElementById("section1").textContent="1.random videos"
+    document.getElementById("uno").textContent="uno game"
+    document.getElementById("poisson").textContent="fish"
+    document.getElementById("backrooms").textContent="bro went in the backrooms"
+    document.getElementById("oiia").textContent="how is he do that"
+    document.getElementById("section2").textContent="2.ASMR transitions"
+    document.getElementById("killer").textContent="CLICK ME"
+    document.getElementById("rickroll").textContent="CLICKE ME TOO"
+}
+function TranslateDe(){
+    document.getElementById("section1").textContent="1.zufällige Videos"
+    document.getElementById("uno").textContent="Teil von Uno"
+    document.getElementById("poisson").textContent="Fische"
+    document.getElementById("backrooms").textContent="Er ging in die Hinterräume"
+    document.getElementById("oiia").textContent="Wie macht er es"
+    document.getElementById("section2").textContent="2.ASMR-Übergänge"
+    document.getElementById("killer").textContent="KLICK MICH"
+    document.getElementById("rickroll").textContent="KLICK AUCH MICH AN"
+}
